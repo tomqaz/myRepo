2274835ade3e1b316b1762a55b7755002311a661
tom <tom@tom-K50AF.(none)>
2013-03-22 18:54:42 +0100

    Czcionki wczytywane z plikow

--- textdrawer.py
+++ textdrawer.py
@@ -5,3 +5,14 @@ class TextDrawer:
 	def draw(self, text):
 		for character in text:
 			print self.font.letters[character.upper()]
+
+    def loadFont(self, directory):
+        font = Font("fancy")
+
+        for filename in os.listdir(directory):
+            font.letters[filename] = readFile(directory+filename)
+
+        return font
+
+    def readFile(filename):
+        return open(filename).read()
