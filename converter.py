4a5aee5b888ad43366670dee8d78f0630655c5e4
tom <tom@tom-K50AF.(none)>
2013-03-22 18:22:03 +0100

    a

---
+++ converter.py
@@ -0,0 +1,19 @@
+from font import FontLoader, Font
+from textdrawer import TextDrawer
+
+def main():
+	text=getText()
+	font=FontLoader().loadFont('fancyFont/')
+	drawer=TextDrawer()
+	drawer.setFont(font)
+	drawer.draw(text)
+
+def getText():
+	return 'Git'
+
+
+
+
+if __name__ == '__main__':
+	main()
+	
